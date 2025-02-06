2dbffe5871e95be614bec7d7fd86337c48ce0487
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-02-06 17:34:21 +0200

    Change Simulation structure: remove TextField

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,11 +2,17 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-export default function Header () {
-    return (
-        <Box id="header-box" component="section">
-            <Typography id="app-main-title" variant="h2" component="h1"
-            align = "center">Asiakasvirtamallinnus</Typography>
-        </Box>
-    )
+export default function Header() {
+  return (
+    <Box id="header-box" component="section">
+      <Typography
+        id="app-main-title"
+        variant="h2"
+        component="h1"
+        align="center"
+      >
+        Asiakasvirtamallinnus
+      </Typography>
+    </Box>
+  );
 }
--- src/Simulation.jsx
+++ src/Simulation.jsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Container, Box, Typography, TextField } from "@mui/material";
 
-export default function Simulation () {
-    return (
-        <Container id="simulation-container">
-            <Box id="initial-queue-input-box">
-                <Typography variant="h5" component="h2" align="center">Syötä jonotilanne!</Typography>
-                <TextField variant="outlined"></TextField>
-            </Box>
-        </Container>
-    );
+export default function Simulation() {
+  return (
+    <Container id="simulation-container">
+      <Box id="initial-queue-input-box">
+        <Typography variant="h5" component="h2" align="center">
+          Syötä jonotilanne!
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
