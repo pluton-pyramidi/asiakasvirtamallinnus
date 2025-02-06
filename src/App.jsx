f873e1f3d41586416f188e63d61bb4d7129f4c38
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-02-06 17:29:19 +0200

    Init new remote

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Header from './Header';
+import Simulation from './Simulation';
+
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Header></Header>
+      <Simulation></Simulation>
+    </Container>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function Header () {
+    return (
+        <Box id="header-box" component="section">
+            <Typography id="app-main-title" variant="h2" component="h1"
+            align = "center">Asiakasvirtamallinnus</Typography>
+        </Box>
+    )
+}
