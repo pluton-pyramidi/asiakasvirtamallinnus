c9115183964a8625eafdde1d1b7daee65f5bf139
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-02-19 12:26:18 +0200

    Strip InputBlockAccordtion.jsx into InputBlock.jsx

--- src/components/InputBlock.jsx
+++ src/components/InputBlock.jsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function InputBlock(props) {
-  <Typography></Typography>;
+  return (
+    <Box>
+      <Typography>Ohjeet</Typography>
+      <TextField label={props.label}></TextField>
+      <TextField label={props.label}></TextField>
+    </Box>
+  );
 }
