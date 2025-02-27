72397507643f95229dd8423397cef9c0528872f5
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-02-27 17:46:11 +0200

    Rename IntegerInput -> InputInteger for consistency

--- src/features/inputInteger/InputInteger.jsx
+++ src/features/inputInteger/InputInteger.jsx
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import * as AllStates from "../../app/store";
 import { Box, Button, Typography, TextField } from "@mui/material";
 
-/* IntegerInput needs props from the parent to 1) give the text field labels and 2) the name of state & its reducer to be used by the state reducer factory in store.js. The state and its reducer need to be configured in store.js */
-export default function IntegerInput({
+/* InputInteger needs props from the parent to 1) give the text field labels and 2) the name of state & its reducer to be used by the state reducer factory in store.js. The state and its reducer need to be configured in store.js */
+export default function InputInteger({
   fieldTitle,
   fieldAriaLabel,
   stateId,
