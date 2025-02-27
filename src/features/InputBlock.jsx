3e24976059f2f9ab8c8ecda33b41ffb67b84a903
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-02-27 16:14:39 +0200

    Refactor the InputBlock view feature

--- src/features/InputBlock.jsx
+++ src/features/InputBlock.jsx
@@ -1,22 +1,95 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
+import { useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import { BoxSx } from "../components/ComponentSx";
 import IntegerInput from "../components/IntegerInput";
-import InputView from "../components/InputView";
 
 export default function InputBlock() {
+  const currentView = useSelector((state) => state.view.currentView);
+
   return (
     <Box sx={BoxSx}>
       <Typography>Ohjeet tähän</Typography>
-      <InputView>
-        <IntegerInput
-          fieldTitle="Syötä jono"
-          fieldAriaLabel="Syötä jonossa olevien potilaiden lukumäärä"
-          stateId="initialQueue"
-          stateSetterId="setInitialQueue"
-        ></IntegerInput>
-      </InputView>
+      <Box>
+        {currentView === "view1" && (
+          <Box>
+            Alkutilanne{" "}
+            <IntegerInput
+              fieldTitle="Syötä jono"
+              fieldAriaLabel="Syötä jonossa olevien potilaiden lukumäärä"
+              stateId="initialQueue"
+              stateSetterId="setInitialQueue"
+            ></IntegerInput>
+          </Box>
+        )}
+        {currentView === "view2" && (
+          <Box>
+            Ensijäsennys{" "}
+            <IntegerInput
+              fieldTitle="asd"
+              fieldAriaLabel=""
+              stateId=""
+              stateSetterId=""
+            ></IntegerInput>
+          </Box>
+        )}
+        {currentView === "view3" && (
+          <Box>
+            Hoitoonohjaus{" "}
+            <IntegerInput
+              fieldTitle="asd"
+              fieldAriaLabel=""
+              stateId=""
+              stateSetterId=""
+            ></IntegerInput>
+          </Box>
+        )}
+        {currentView === "view4" && (
+          <Box>
+            TAU{" "}
+            <IntegerInput
+              fieldTitle="asd"
+              fieldAriaLabel=""
+              stateId=""
+              stateSetterId=""
+            ></IntegerInput>
+          </Box>
+        )}
+        {currentView === "view5" && (
+          <Box>
+            P1{" "}
+            <IntegerInput
+              fieldTitle="asd"
+              fieldAriaLabel=""
+              stateId=""
+              stateSetterId=""
+            ></IntegerInput>
+          </Box>
+        )}
+        {currentView === "view6" && (
+          <Box>
+            P2{" "}
+            <IntegerInput
+              fieldTitle="asd"
+              fieldAriaLabel=""
+              stateId=""
+              stateSetterId=""
+            ></IntegerInput>
+          </Box>
+        )}
+        {currentView === "view7" && (
+          <Box>
+            Muu{" "}
+            <IntegerInput
+              fieldTitle="asd"
+              fieldAriaLabel=""
+              stateId=""
+              stateSetterId=""
+            ></IntegerInput>
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 }
