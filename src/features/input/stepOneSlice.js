7c1c61988591ee12fc5382a8cf9dd56295e9c605
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-03-09 15:34:20 +0200

    add stepOne params to stepOneSlice

--- src/features/input/stepOneSlice.js
+++ src/features/input/stepOneSlice.js
@@ -4,6 +4,11 @@ const initialState = {
   hoitoonohjausStepOne: 0.5,
   laborStepOne: 5,
   laborPercentageStepOne: 0.5,
+  sufficiencyRateStepOne: 0.5,
+  stepOneToStepTwoRate: 0.6,
+  stepOneToMuuRate: 0.2,
+  meetingDurationStepOne: 90,
+  laborEfficiencyStepOne: 0.8,
 };
 
 const stepOneSlice = createSlice({
@@ -14,13 +19,18 @@ const stepOneSlice = createSlice({
       state.hoitoonohjausStepOne = action.payload.hoitoonohjausStepOne;
       state.laborStepOne = action.payload.laborStepOne;
       state.laborPercentageStepOne = action.payload.laborPercentageStepOne;
+      state.sufficiencyRateStepOne = action.payload.sufficiencyRateStepOne;
+      state.stepOneToStepTwoRate = action.payload.stepOneToStepTwoRate;
+      state.stepOneToMuuRate = action.payload.stepOneToMuuRate;
+      state.meetingDurationStepOne = action.payload.meetingDurationStepOne;
+      state.laborEfficiencyStepOne = action.payload.laborEfficiencyStepOne;
     },
   },
 });
 
 export const { setStepOne } = stepOneSlice.actions;
 
-// Selector to derive hoitoonohjausStepTwo
+// Selectors to derive variables from this state
 export const calculateHoitoonohjausStepTwo = (state) =>
   1 - state.stepOne.hoitoonohjausStepOne;
 
