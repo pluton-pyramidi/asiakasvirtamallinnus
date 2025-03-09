5ef872c49878ae7e20a3f69ff1d0ee4d0c41f838
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-03-09 16:02:13 +0200

    add tau params to tauSlice

--- src/features/input/tauSlice.js
+++ src/features/input/tauSlice.js
@@ -3,6 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   laborTau: 5,
   laborPercentageTau: 0.5,
+  sufficiencyRateTau: 0.45,
+  tauToMuuRate: 0.5,
+  meetingDurationTau: 90,
+  laborEfficiencyTau: 0.7,
 };
 
 const tauSlice = createSlice({
@@ -12,9 +16,22 @@ const tauSlice = createSlice({
     setTau: (state, action) => {
       state.laborTau = action.payload.laborTau;
       state.laborPercentageTau = action.payload.laborPercentageTau;
+      state.sufficiencyRateTau = action.payload.sufficiencyRateTau;
+      state.tauToMuuRate = action.payload.tauToMuuRate;
+      state.meetingDurationTau = action.payload.meetingDurationTau;
+      state.laborEfficiencyTau = action.payload.laborEfficiencyTau;
     },
   },
 });
 
 export const { setTau } = tauSlice.actions;
+
+// Selectors to derive variables from this state
+export const calculateInsufficencyRateTau = (state) =>
+  1 - state.tau.sufficiencyRateTau;
+
+export const calculateTauToQueueRate = (state) => {
+  1 - state.tau.tauToMuuRate;
+};
+
 export default tauSlice.reducer;
