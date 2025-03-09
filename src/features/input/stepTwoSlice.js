8947ba3f05beb0658f995a9ead0384328687045e
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-03-09 15:57:59 +0200

    add stepTwo params to stepTwoSlice

--- src/features/input/stepTwoSlice.js
+++ src/features/input/stepTwoSlice.js
@@ -3,6 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   laborStepTwo: 5,
   laborPercentageStepTwo: 0.5,
+  sufficiencyRateStepTwo: 0.5,
+  stepTwoToMuuRate: 0.5,
+  meetingDurationStepTwo: 90,
+  laborEfficiencyStepTwo: 0.8,
 };
 
 const stepTwoSlice = createSlice({
@@ -12,9 +16,22 @@ const stepTwoSlice = createSlice({
     setStepTwo: (state, action) => {
       state.laborStepTwo = action.payload.laborStepTwo;
       state.laborPercentageStepTwo = action.payload.laborPercentageStepTwo;
+      state.sufficiencyRateStepTwo = action.payload.sufficiencyRateStepTwo;
+      state.stepTwoToMuuRate = action.payload.stepTwoToMuuRate;
+      state.meetingDurationStepTwo = action.payload.meetingDurationStepTwo;
+      state.laborEfficiencyStepTwo = action.payload.laborEfficiencyStepTwo;
     },
   },
 });
 
 export const { setStepTwo } = stepTwoSlice.actions;
+
+// Selectors to derive variables from this state
+export const calculateInsufficencyRateStepTwo = (state) =>
+  1 - state.stepTwo.sufficiencyRateStepTwo;
+
+export const calculateStepTwoToQueueRate = (state) => {
+  1 - state.stepTwo.stepTwoToMuuRate;
+};
+
 export default stepTwoSlice.reducer;
