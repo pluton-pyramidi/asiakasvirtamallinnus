03d1e619655617d7d3fb112caad8cedea72e5f9f
Alex Kiril Casimir Villa <alex.kc.villa@protonmail.com>
2025-04-08 15:56:16 +0300

    Refactor handleSubmit into a reusable component which also runs the simulation

---
+++ src/utils/handleSubmitAndRun.js
@@ -0,0 +1,13 @@
+import { calculateSimulationBalance } from "../features/run/balanceSlice";
+
+/**
+ * A reusable function to handle submitting input to a Redux reducer.
+ * @param {Function} dispatch - The Redux dispatch function.
+ * @param {Object} input - The input data to be dispatched.
+ * @param {Function} setAction - The Redux action creator (e.g., setPatientInput).
+ */
+export const handleSubmitAndRun = (dispatch, input, setAction) => {
+  console.log("Dispatching input state:", input);
+  dispatch(setAction(input)); // Dispatch the input to the specified reducer
+  dispatch(calculateSimulationBalance());
+};
