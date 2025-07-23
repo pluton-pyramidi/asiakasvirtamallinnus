4e56effe3faf7a713069b62099342c568a0ab3e8
Villa Alex <alex.villa@hus.fi>
2025-07-23 16:32:07 +0300

    rebuild LineChart from example

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,31 +1,129 @@
-import * as React from "react";
 import * as d3 from "d3";
-import { useSelector } from "react-redux";
 
-export default function LineChart({
-  width = 640,
-  height = 400,
-  marginTop = 20,
-  marginRight = 20,
-  marginBottom = 20,
-  marginLeft = 20,
-}) {
-  // Get queue data from store
-  const simulatedQueueData = useSelector((state) => state.balance.value);
-  const dataLength = simulatedQueueData.length;
+export const LineChartDraw = (
+  data,
+  {
+    x = ([x]) => x, // given d in data, returns the (temporal) x-value
+    y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
+    defined, // for gaps in data
+    curve = d3.curveLinear, // method of interpolation between points
+    marginTop = 20, // top margin, in pixels
+    marginRight = 30, // right margin, in pixels
+    marginBottom = 30, // bottom margin, in pixels
+    marginLeft = 40, // left margin, in pixels
+    width = 640, // outer width, in pixels
+    height = 400, // outer height, in pixels
+    xType = d3.scaleLinear, // type of x-scale
+    xDomain, // [xmin, xmax]
+    xRange = [marginLeft, width - marginRight], // [left, right]
+    yType = d3.scaleLinear, // type of y-scale
+    yDomain, // [ymin, ymax]
+    yRange = [height - marginBottom, marginTop], // [bottom, top]
+    yFormat, // a format specifier string for the y-axis
+    color = "currentColor", // stroke color of line, as a constant or a function of *z*
+  } = {}
+) => {
+  // Compute values.
+  const X = d3.map(data, x);
+  const Y = d3.map(data, y);
+  if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
+  const D = d3.map(data, defined);
 
-  const x = d3.scaleLinear([0, dataLength - 1], [marginLeft, width - marginRight]);
-  const y = d3.scaleLinear([0, d3.max(simulatedQueueData)], [height - marginBottom, marginTop]);
-  const line = d3.line((d, i) => x(i), y);
+  // Compute default x- and y-domains
+  if (xDomain === undefined) xDomain = d3.extent(X);
+  if (yDomain === undefined) yDomain = [0, d3.max(Y, (d) => (typeof d === "string" ? +d : d))];
 
-  return (
-    <svg width={width} height={height}>
-      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(simulatedQueueData)} />
-      <g fill="white" stroke="currentColor" strokeWidth="1.5">
-        {simulatedQueueData.map((d, i) => (
-          <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
-        ))}
-      </g>
-    </svg>
-  );
-}
+  // Construct scales and axes.
+  const xScale = xType(xDomain, xRange);
+  const yScale = yType(yDomain, yRange);
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(width / 80)
+    .tickSizeOuter(0);
+  const yAxis = d3.axisLeft(yScale).ticks(height / 60, yFormat);
+
+  // Construct a line generator.
+  const line = d3
+    .line()
+    .defined((i) => D[i])
+    .curve(curve)
+    .x((i) => xScale(X[i]))
+    .y((i) => yScale(Y[i]));
+
+  const svg = d3
+    .create("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+    .style("-webkit-tap-highlight-color", "transparent")
+    .on("pointerenter", pointerentered)
+    .on("pointermove", pointermoved)
+    .on("pointerleave", pointerleft)
+    .on("touchstart", (event) => event.preventDefault());
+
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(xAxis);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(yAxis)
+    .call((g) => g.select(".domain").remove())
+    .call((g) =>
+      g.append("text").attr("x", -marginLeft).attr("y", 10).attr("fill", "currentColor").attr("text-anchor", "start")
+    );
+
+  const path = svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", typeof color === "string" ? color : null)
+    .attr("stroke-linecap", strokeLinecap)
+    .attr("stroke-linejoin", strokeLinejoin)
+    .attr("stroke-width", strokeWidth)
+    .attr("stroke-opacity", strokeOpacity)
+    .selectAll("path")
+    .data(d3.group(I, (i) => Z[i]))
+    .join("path")
+    .style("mix-blend-mode", mixBlendMode)
+    .attr("stroke", typeof color === "function" ? ([z]) => color(z) : null)
+    .attr("d", ([, I]) => line(I));
+  const dot = svg.append("g").attr("display", "none");
+
+  dot.append("circle").attr("r", 2.5);
+
+  dot
+    .append("text")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 10)
+    .attr("text-anchor", "middle")
+    .attr("y", -8);
+
+  function pointermoved(event) {
+    const [xm, ym] = d3.pointer(event);
+    const i = d3.least(I, (i) => Math.hypot(xScale(X[i]) - xm, yScale(Y[i]) - ym)); // closest point
+    path
+      .style("stroke", ([z]) => (Z[i] === z ? null : "#ddd"))
+      .filter(([z]) => Z[i] === z)
+      .raise();
+    dot.attr("transform", `translate(${xScale(X[i])},${yScale(Y[i])})`);
+    if (T) dot.select("text").text(T[i]);
+    svg.property("value", O[i]).dispatch("input", { bubbles: true });
+  }
+
+  function pointerentered() {
+    path.style("mix-blend-mode", null).style("stroke", "#ddd");
+    dot.attr("display", null);
+  }
+
+  function pointerleft() {
+    path.style("mix-blend-mode", mixBlendMode).style("stroke", null);
+    dot.attr("display", "none");
+    svg.node().value = null;
+    svg.dispatch("input", { bubbles: true });
+  }
+
+  return Object.assign(svg.node(), { value: null });
+};
