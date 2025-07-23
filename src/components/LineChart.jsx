e26387e05f4368a0a02b3fa3d97503481064db1c
Villa Alex <alex.villa@hus.fi>
2025-07-23 12:51:24 +0300

    rebuild chart using d3

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as d3 from "d3";
+import { useSelector } from "react-redux";
+
+export default function LineChart({
+  width = 640,
+  height = 400,
+  marginTop = 20,
+  marginRight = 20,
+  marginBottom = 20,
+  marginLeft = 20,
+}) {
+  // Get queue data from store
+  const simulatedQueueData = useSelector((state) => state.balance.value);
+  const dataLength = simulatedQueueData.length;
+
+  const x = d3.scaleLinear([0, dataLength - 1], [marginLeft, width - marginRight]);
+  const y = d3.scaleLinear([0, d3.max(simulatedQueueData)], [height - marginBottom, marginTop]);
+  const line = d3.line((d, i) => x(i), y);
+
+  return (
+    <svg width={width} height={height}>
+      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(simulatedQueueData)} />
+      <g fill="white" stroke="currentColor" strokeWidth="1.5">
+        {simulatedQueueData.map((d, i) => (
+          <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
+        ))}
+      </g>
+    </svg>
+  );
+}
--- src/features/chart/QueueChart.jsx
+++ src/features/chart/QueueChart.jsx
@@ -1,30 +1,10 @@
-import * as React from "react";
-import { useSelector } from "react-redux";
 import { Box } from "@mui/material";
-import { BoxSx } from "../../utils/ComponentSx";
-import { LineChart } from "@mui/x-charts/LineChart";
+import LineChart from "../../components/LineChart.jsx";
 
 export default function QueueChart() {
-  // Get the simulated queue data from store
-  const simulatedQueueData = useSelector((state) => state.balance.value);
-  const dataLength = simulatedQueueData.length;
-  // Initialize x-axis by creating an array of the same length as the data
-  const xAxis = Array.from({ length: dataLength }, (_, i) => i + 1);
-
   return (
     <Box>
-      <LineChart
-        width={500}
-        height={300}
-        series={[
-          {
-            data: simulatedQueueData,
-            label: "Jonotilanteen mallinnettu kehitys",
-          },
-        ]}
-        xAxis={[{ scaleType: "point", data: xAxis }]}
-        yAxis={[{ min: 0 }]}
-      ></LineChart>
+      <LineChart></LineChart>
     </Box>
   );
 }
