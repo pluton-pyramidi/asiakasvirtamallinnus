00b5ea7f4dbf8009552604566aef3133e8acf5a3
Villa Alex <alex.villa@hus.fi>
2025-07-25 13:43:37 +0300

    start building d3 process unit component

--- src/components/ProcessUnit.jsx
+++ src/components/ProcessUnit.jsx
@@ -3,6 +3,6 @@ import * as d3 from "d3";
 import { NumberField } from "@base-ui-components/react/number-field";
 import styles from "../utils/numberField.module.css";
 
-export default function VisualTreatmentBox(props) {
+export const VisualTreatmentBox = (props) => {
   return <svg> </svg>;
-}
+};
